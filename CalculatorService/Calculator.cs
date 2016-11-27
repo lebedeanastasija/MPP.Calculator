360258d815e0506a4c6e46e7b850873c2609c62c
Ivan <vana.shimko@gmail.com>
2016-11-27 13:33:04 +0300

    Add calculator WCF service with contract

---
+++ CalculatorService/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorService
+{
+    public class Calculator : ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Substract(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Multiply(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double a, double b)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Sqrt(double a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CalculatorService/ICalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CalculatorService
+{    
+    [ServiceContract(Name = "CalculatorService", Namespace = "bsuir.mpp/IvanShimko/2016/11")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double a, double b);
+
+        [OperationContract]
+        double Substract(double a, double b);
+
+        [OperationContract]
+        double Multiply(double a, double b);
+
+        [OperationContract]
+        double Divide(double a, double b);
+
+        [OperationContract]
+        double Sqrt(double a);
+    }    
+}
